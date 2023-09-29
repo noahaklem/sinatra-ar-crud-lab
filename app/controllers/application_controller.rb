043a289ce26e7ffa237e6e8bf1fffb9105cead87
Noah A Klem <noahaklem@gmail.com>
2023-09-29 12:21:36 -0600

    crud actions sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,59 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
   get '/' do
+    redirect to "/articles"
   end
+
+  #ERB index
+  get '/articles' do 
+    @articles = Article.all
+  
+    erb :index
+  end
+
+  #ERB new
+  get '/articles/new' do
+     @article = Article.new
+  
+    erb :new
+  end
+
+  #CREATE
+  post '/articles' do
+    @article = Article.create(params)
+
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #ERB show
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  #ERB Edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  #UPDATE
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #DELETE
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+  
 end
